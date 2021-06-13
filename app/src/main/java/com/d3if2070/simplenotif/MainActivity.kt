a2992d3673c1e205cca62ba9f179292c3527eb10
Primanda Sayarizki <primanda.sayarizki8@gmail.com>
2021-06-13 15:29:05 +0700

    Menambahkan Pending Intent

--- app/src/main/java/com/d3if2070/simplenotif/MainActivity.kt
+++ app/src/main/java/com/d3if2070/simplenotif/MainActivity.kt
@@ -2,8 +2,11 @@ package com.d3if2070.simplenotif
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -27,8 +30,12 @@ class MainActivity : AppCompatActivity() {
 
     fun sendNotification(view : View) {
 
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://youtube.com"))
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentIntent(pendingIntent)
                 .setSmallIcon(R.drawable.ic_baseline_notifications_24)
                 .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_baseline_notifications_24))
                 .setContentTitle(resources.getString(R.string.content_title))
